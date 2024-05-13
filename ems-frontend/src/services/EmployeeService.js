d25651021efc526c9d0ff6c5b31a1b6ff8ec600b
barthmalemew <kevinrouse105@gmail.com>
2024-05-13 14:57:41 -0400

    Full Conversion to node.js

--- ems-frontend/src/services/EmployeeService.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const REST_API_BASE_URL = 'http://localhost:8080/drivers';
-
-export const listEmployees = () => axios.get(REST_API_BASE_URL);
--- src/main/java/edu/ecu/ecutransit/repository/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.ecu.ecutransit.repository;
-
-import edu.ecu.ecutransit.entity.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-}
